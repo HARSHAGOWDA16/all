ca50affaea1160f6b7e4fb017c70c7c86910999d
HG1624 <62892093+HG1624@users.noreply.github.com>
2021-04-16 10:16:58 +0530

    Add files via upload

---
+++ code/ok.py
@@ -0,0 +1,51 @@
+import pyttsx3
+engine = pyttsx3.init()
+import os
+from gtts import gTTS
+
+fh = open("test.txt","r")
+myText = fh.read().replace("\n"," ")
+language="en"
+output=gTTS(text=myText,lang=language,slow=False)
+
+output.save("output.mp3")
+fh.close()
+os.system("start output.mp3")
+engine.say("I will speak this text")
+engine.runAndWait()
+
+
+	
+
+
+import pyttsx3
+engine = pyttsx3.init() # object creation
+
+""" RATE"""
+rate = engine.getProperty('rate')   # getting details of current speaking rate
+print (rate)                        #printing current voice rate
+engine.setProperty('rate', 125)     # setting up new voice rate
+
+
+"""VOLUME"""
+volume = engine.getProperty('volume')   #getting to know current volume level (min=0 and max=1)
+print (volume)                          #printing current volume level
+engine.setProperty('volume',1.0)    # setting up volume level  between 0 and 1
+
+"""VOICE"""
+voices = engine.getProperty('voices')       #getting details of current voice
+#engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
+engine.setProperty('voice', voices[1].id)   #changing index, changes voices. 1 for female
+
+engine.say("Hello World!")
+engine.say('My current speaking rate is ' + str(rate))
+engine.runAndWait()
+engine.stop()
+
+
+"""Saving Voice to a file"""
+# On linux make sure that 'espeak' and 'ffmpeg' are installed
+engine.save_to_file('Hello World', 'test.mp3')
+engine.runAndWait()
+
+
